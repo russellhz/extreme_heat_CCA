8879e7289cb8e9ee9c5acb3d18d01992c0eeb60d
Russell Horowitz <horowitz.russell@gmail.com>
2021-01-01 19:30:02 -0800

    naming in anom_AMJJAS

--- scriptrunner/L0_scriptrunner_tas_slp_anom_AMJJAS_creator.py
+++ scriptrunner/L0_scriptrunner_tas_slp_anom_AMJJAS_creator.py
@@ -5,7 +5,7 @@ import os
 LOCS = ["Northeast", "NorthernRockiesandPlains", "Northwest", "OhioValley", "South", "Southeast", "Southwest", "UpperMidwest", "West"]  
 
 for LOC in LOCS:
-    job_file = "jobs/" + 'tas_anom_AMJJAS_creator_'+ LOC +'.sbatch'
+    job_file = "jobs/" + 'tas_slp_anom_AMJJAS_creator_'+ LOC +'.sbatch'
     open(job_file, 'a')
 
     with open(job_file, "w") as fh:
@@ -16,11 +16,11 @@ for LOC in LOCS:
         fh.writelines("#SBATCH --time=03:00:00\n")
         fh.writelines("#SBATCH --partition=dav\n")
         fh.writelines("#SBATCH --mem=100G\n")
-        fh.writelines("#SBATCH --output=out/tas_anom_AMJJAS.out.%j\n")
+        fh.writelines("#SBATCH --output=out/tas_slp_anom_AMJJAS.out.%j\n")
         fh.writelines("module load ncarenv\n")
         fh.writelines("module load python\n")
         fh.writelines("ncar_pylib my_npl_clone_casper\n")
-        fh.writelines("python ../py/L0/tas_anom_AMJJAS_creator.py " + LOC + " > logs/tas_anom_AMJJAS_creator_" + LOC+ ".log\n")
+        fh.writelines("python ../py/L0/tas_slp_anom_AMJJAS_creator.py " + LOC + " > logs/tas_slp_anom_AMJJAS_creator_" + LOC+ ".log\n")
         fh.writelines("deactivate\n")
         
     os.system("sbatch %s" %job_file)
