b7aa3a4fb4db903275797df3c13dea03672fbed9
Russell Horowitz <horowitz.russell@gmail.com>
2021-01-10 10:26:17 -0800

    cesmle_slp_correlation

---
+++ py/L1/cesmle_slp_correlation.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: rhorowitz
+"""
+import sys
+import xarray as xr
+import os
+import glob
+import re
+import numpy as np
+import pandas as pd
+import argparse
+import geopandas
+import salem
+import copy
+import cftime
+import random
+
+
+sys.path.append("/glade/u/home/horowitz/extreme_heat_CCA/py/L0")
+import L0_functions as L0
+
+os.chdir("/glade/u/home/horowitz/extreme_heat_CCA/py/L1")
+import L1_functions as L1
+
+DIR = "/glade/work/horowitz/extreme_heat_CCA/L1/"
+CESMLE_DIR = "/glade/scratch/horowitz/extreme_heat_CCA/JJA_anom/"
+
+LOCS = ["Northeast", "NorthernRockiesandPlains", "Northwest", "OhioValley", "South", 
+        "Southeast", "Southwest", "UpperMidwest", "West"]  
+PICTL_TYPE = "PICTL"
+
+##############################################################################
+################          Average SLP heatwave pattern        ################
+##############################################################################
+slp_data_dict = {}
+for LOC in LOCS:
+    fn = DIR + 'gridded_slp_z500/gridded_slp_z500_' + PICTL_TYPE + '_' + LOC + '.nc'
+    ################## OPEN DATA ######################        
+    data = xr.open_dataset(fn).mean(dim='time')
+    slp_data_dict[LOC] = data.slp 
+
+##############################################################################
+################             CESM-LE Correlation              ################
+##############################################################################
+
+cesmle_fns = [f for f in glob.glob(CESMLE_DIR + "CESM-LE_PSL_anom_" + "*" + "19200101-21001231.nc")]
+cesmle_fns = np.sort(cesmle_fns)  
+
+df_all = pd.DataFrame()
+
+
+for fn in cesmle_fns:
+    code = re.search(r'(?<=_)[0-9]{3}(?=_)', fn).group()
+    data = xr.open_dataset(fn)
+    data['lat'] = np.round(data['lat'], 2)
+    for LOC in LOCS:
+        heatwave = slp_data_dict[LOC]
+        # Filter data to location
+        data_loc = data.salem.subset(corners = ((heatwave.lon.min(),heatwave.lat.min()), 
+                                                (heatwave.lon.max(),heatwave.lat.max())))
+        nlat = heatwave.shape[0]
+        nlon = heatwave.shape[1]
+        ndays = data_loc.anom.shape[0]
+
+        heatwave = heatwave.values.reshape((nlat*nlon)).reshape(1, -1)
+        data_loc = data_loc.anom.values.reshape((ndays, nlat*nlon))
+
+        corr = L1.corr2_coeff(heatwave, data_loc)
+        df_loc = pd.DataFrame({'region':LOC, 'code': code, 'date':data.time.values, 'correlation':corr[0,:]})
+        df_all = pd.concat([df_all, df_loc])
+    print("Done with " + code)
+
+df_all.to_csv(DIR+'cesmle_slp_correlation.csv')
