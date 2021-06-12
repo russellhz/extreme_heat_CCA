e429b6c3c95d605a9d7c5221295d3ef8e60178e9
Russell Horowitz <horowitz.russell@gmail.com>
2021-06-12 15:02:26 -0700

    converting to pbs

--- scriptrunner/L1_scriptrunner_gridded_slp_z500.py
+++ scriptrunner/L1_scriptrunner_gridded_slp_z500.py
@@ -7,18 +7,17 @@ open(job_file, 'a')
 
 with open(job_file, "w") as fh:
     fh.writelines("#!/bin/bash -l\n")
-    fh.writelines("#SBATCH --job-name=gridded_slp_z500\n")
-    fh.writelines("#SBATCH --account=P04010022\n")
-    fh.writelines("#SBATCH --ntasks=1\n")
-    fh.writelines("#SBATCH --time=1:00:00\n")
-    fh.writelines("#SBATCH --partition=dav\n")
-    fh.writelines("#SBATCH --output=out/gridded_slp_z500.out.%j\n")
+    fh.writelines("#PBS -q casper\n")
+    fh.writelines("#PBS -N gridded_slp_z500\n")
+    fh.writelines("#PBS -A P04010022\n")
+    fh.writelines("#PBS -l walltime=01:00:00\n")
+    fh.writelines("#PBS -o out/gridded_slp_z500.out\n")
     fh.writelines("module load ncarenv\n")
     fh.writelines("module load python\n")
     fh.writelines("ncar_pylib my_npl_clone_casper\n")
     fh.writelines("python ../py/L1/gridded_slp_z500.py "  + " > logs/gridded_slp_z500"  + ".log\n")
     fh.writelines("deactivate\n")
     
-os.system("sbatch %s" %job_file)
+os.system("qsub %s" %job_file)
 
 
