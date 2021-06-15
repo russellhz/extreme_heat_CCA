49fd8c173c09aa50aedbbded75de71eb2a9cdd7c
Russell Horowitz <horowitz.russell@gmail.com>
2021-06-15 15:58:15 -0700

    lagged distance matrix

---
+++ py/L0/distance_matrix_LAG.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import xarray as xr
+import os
+import glob
+import re
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import euclidean_distances
+import random
+import argparse
+import math
+
+# Directories
+os.chdir("/glade/u/home/horowitz/extreme_heat_CCA/py/L0")
+DIR = "/glade/scratch/horowitz/extreme_heat_CCA/AMJJAS_anom/"
+ODIR = "/glade/scratch/horowitz/extreme_heat_CCA/distance_matrices/"
+
+import L0_functions as L0
+
+# Parser arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('LOC', type=str, help='region')
+parser.add_argument('PICTL', type=str, help='PiCTL simulation')
+parser.add_argument('LAG', type=int, help='Days to lag by')
+args = parser.parse_args()
+
+LOC = args.LOC
+PICTL = args.PICTL
+LAG = args.LAG
+
+# Create indices for each day of MJJA
+# 183 days in AMJJAS
+# 123 days in MJJA
+summer_dict = {}
+all_days = list(range(1799*183))
+days_by_year = np.array_split(np.array(all_days), 1799) # split into array for each year
+
+for i in range(123):
+    # Should be comparing to 7 days before (23 days more than i because April has 30 days) until
+    # 7 days after (37 days after i)
+    days_to_compare = list(range(i+23, i+38))
+    other_years = [x[days_to_compare] for x in days_by_year]
+    summer_dict[i] = [item for sublist in other_years for item in sublist]
+
+
+print("Starting " + LOC + ' for ' + PICTL)
+# PICTL Files
+slp_pictl_fn = DIR + "PSL_" + PICTL + "_"  + LOC + "_AMJJAS_anom.nc"
+
+##################### LOAD VAR DATA ##################
+slp_pictl = xr.open_dataset(slp_pictl_fn)
+slp_pictl['lat']  =  np.round(slp_pictl.lat, 2)
+print("slp files loaded")
+
+slp_pictl = slp_pictl.shift(time=LAG)
+print("slp lagged")
+
+
+###################### FILTER DATA ###############################
+# Filter to JMJA
+slp_MJJA = slp_pictl.anom.sel(time = slp_pictl.time.dt.month.isin([5,6,7,8])).values
+
+ndays = slp_MJJA.shape[0]
+nlat = slp_MJJA.shape[1]
+nlon = slp_MJJA.shape[2]
+nyear = ndays/123
+
+slp_MJJA = slp_MJJA.reshape((ndays, nlat*nlon))
+
+print("MJJA reshaped")
+
+# Save AMJJAS data
+slp_AMJJAS = slp_pictl.anom.values
+
+ndays = slp_AMJJAS.shape[0]
+
+slp_AMJJAS = slp_AMJJAS.reshape((ndays, nlat*nlon))
+
+print("AMJJAS reshaped")
+
+for i in range(123):
+    # Find scores between JJA day and relevant MJJAS days
+    scores = euclidean_distances(slp_MJJA[range(i, 1799*123, 123)], slp_AMJJAS[summer_dict[i]])
+    comp_length = int(scores.shape[1]/1799)
+    scores_fixed = np.empty((scores.shape[0],scores.shape[1]-comp_length))
+    # Remove distances between same year
+    for j in range(scores.shape[0]):
+        remove_idx = range(j*comp_length, j*comp_length+comp_length)
+        keep_idk = list(set(range(0,scores.shape[1])) - set(remove_idx))
+        keep_idk.sort()
+        scores_fixed[j,:] = scores[j,keep_idk]
+    np.save(ODIR + 'distance_matrix_' + PICTL + '_' + LOC + '_LAG' + LAG + '_' + str(i), scores_fixed)
+    if i%10==0:
+        print("Done with " + str(i))
+print("--------------------------------------------")
---
+++ scriptrunner/L0_scriptrunner_distance_matrix_LAG.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+
+LOCS = ["Northeast", "NorthernRockiesandPlains", "Northwest", "OhioValley", "South", "Southeast", "Southwest", "UpperMidwest", "West"]  
+PICTLS = ['PICTL', 'SSTPICTL']
+LAGS = [1,3]
+
+for LOC in LOCS:
+    for PICTL in PICTLS:
+        for LAG in LAGS:
+            args = " ".join([LOC,PICTL, LAG])
+            args_ = "_".join([LOC,PICTL, LAG])
+
+            job_file = "jobs/distance_matrix_" + args_ + '.sbatch'
+            open(job_file, 'a')
+
+            with open(job_file, "w") as fh:
+                fh.writelines("#!/bin/bash -l\n")
+                fh.writelines("#PBS -q casper\n")
+                fh.writelines("#PBS -N distance_matrix_LAG\n")
+                fh.writelines("#PBS -A P04010022\n")
+                fh.writelines("#PBS -l select=1\n")
+                fh.writelines("#PBS -l walltime=03:00:00\n")
+                fh.writelines("#PBS -o out/distance_matrix.out\n")
+                
+                fh.writelines("module load ncarenv\n")
+                fh.writelines("module load python\n")
+                fh.writelines("ncar_pylib my_npl_clone_casper\n")
+                fh.writelines("python ../py/L0/distance_matrix_LAG.py "  + args + " > logs/distance_matrix_LAG" + args_ +".log\n")
+                fh.writelines("deactivate\n")
+                
+            os.system("qsub %s" %job_file)
+
+
